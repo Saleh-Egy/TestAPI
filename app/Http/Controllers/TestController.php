5d91f8ca65506f5dd83d8355f27465ef4aa4604d
Saleh-Egy <mosaleh2018aast@gmail.com>
2021-04-18 15:37:36 +0200

    first commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Test;
+use App\Http\Requests\TestRequest;
+use App\Http\Resources\TestResource;
+
+use DB;
+
+class TestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(TestRequest $request)
+    {
+        try{
+
+            // Start Try DB but Not Confirm
+            DB::beginTransaction();
+
+
+                // code
+
+
+            // To Confirm DB actions and Save
+            DB::commit();
+
+        }catch(\Exception $e){
+            // Database Rollback
+            DB::rollback();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Resources/TestResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TestResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name'=>$this->name,
+            'slug'=>$this->slug,
+            'description'=>$this->description !== 'null' ?? 'Sorry,There Is No Description Avaliable.',
+        ];
+    }
+}
